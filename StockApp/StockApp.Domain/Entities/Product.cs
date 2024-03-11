b3da07f5f968abab163c1e8280a616451c90c845
victoricoma <victor.icoma@etec.sp.gov.br>
2024-03-11 12:31:37 -0300

    Aula 1103 - Validação parcial dos dominios

--- StockApp/StockApp.Domain/Entities/Product.cs
+++ StockApp/StockApp.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockApp.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace StockApp.Domain.Entities
 {
     public class Product
     {
+        #region Atributos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
@@ -15,7 +17,29 @@ namespace StockApp.Domain.Entities
         public int Stock { get; set;}
         public string Image { get; set; }
         public int CategoryId { get; set; }
+        #endregion
+
+        public Product()
+        {
+
+        }
 
         public Category Category { get; set; }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name, name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Invalid description, name is required.");
+
+            DomainExceptionValidation.When(description.Length < 5,
+                "Invalid description, too short, minimum 5 characters.");
+
+        }
     }
 }
