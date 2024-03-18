46aba4acc67a7c9d11c05c30ba720f1ed902a6d0
victoricoma <victor.icoma@etec.sp.gov.br>
2024-03-18 12:33:25 -0300

    Aula1803 - Validações Restantes

--- StockApp/StockApp.Domain/Entities/Product.cs
+++ StockApp/StockApp.Domain/Entities/Product.cs
@@ -40,6 +40,12 @@ namespace StockApp.Domain.Entities
             DomainExceptionValidation.When(description.Length < 5,
                 "Invalid description, too short, minimum 5 characters.");
 
+            DomainExceptionValidation.When(price < 0, "Invalid price negative value.");
+
+            DomainExceptionValidation.When(stock < 0, "Invalid stock negative value.");
+
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, maximum 250 characters.");
+
         }
     }
 }
